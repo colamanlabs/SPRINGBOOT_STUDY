db6f2cf939e7d0cbb0179a3ebe1e2a4fbe16e017
DEFAULT3\colaman <colaman@nate.com>
2024-02-08 22:55:46 +0900

    최초 프로젝트 생성

---
+++ springboot_study_0001/src/main/java/com/colamanlabs/springbootstudy/s0001/SpringbootStudy0001Application.java
@@ -0,0 +1,13 @@
+package com.colamanlabs.springbootstudy.s0001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudy0001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudy0001Application.class, args);
+	}
+
+}
---
+++ springboot_study_0001/src/test/java/com/colamanlabs/springbootstudy/s0001/SpringbootStudy0001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colamanlabs.springbootstudy.s0001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudy0001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
