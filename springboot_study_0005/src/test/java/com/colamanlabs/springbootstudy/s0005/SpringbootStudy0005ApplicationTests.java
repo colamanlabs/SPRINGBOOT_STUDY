faf27fa0fbc79cdbf7303eaef91c0faacdff1138
DEFAULT3\colaman <colaman@nate.com>
2024-02-14 23:41:55 +0900

    스프링배치 검토를 위한 최초 프로젝트 설정

---
+++ springboot_study_0005/src/test/java/com/colamanlabs/springbootstudy/s0005/SpringbootStudy0005ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colamanlabs.springbootstudy.s0005;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudy0005ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
