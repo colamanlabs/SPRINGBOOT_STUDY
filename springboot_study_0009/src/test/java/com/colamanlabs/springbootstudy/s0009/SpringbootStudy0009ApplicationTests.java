dc22e5fcd5e39696a9266827ae23badd5269fc9c
DEFAULT3\colaman <colaman@nate.com>
2024-02-16 23:57:33 +0900

    환경구성

---
+++ springboot_study_0009/src/test/java/com/colamanlabs/springbootstudy/s0009/SpringbootStudy0009ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colamanlabs.springbootstudy.s0009;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudy0009ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
