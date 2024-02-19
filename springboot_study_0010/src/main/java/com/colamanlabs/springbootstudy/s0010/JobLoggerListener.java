4abd22518d0c0470e3f7e2d0e42deccc224d41b7
DEFAULT3\colaman <colaman@nate.com>
2024-02-19 23:12:27 +0900

    환경구성

---
+++ springboot_study_0010/src/main/java/com/colamanlabs/springbootstudy/s0010/JobLoggerListener.java
@@ -0,0 +1,32 @@
+package com.colamanlabs.springbootstudy.s0010;
+
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JobLoggerListener implements JobExecutionListener
+{
+    
+    @Override
+    public void beforeJob(JobExecution jobExecution)
+    {
+        // TODO Auto-generated method stub
+        String jobName = jobExecution.getJobInstance().getJobName();
+        String strLog = String.format("[%s] is beginning execution", jobName);
+        log.info(String.format("[JobLoggerListener/beforeJob] %s", strLog));
+    }
+    
+    @Override
+    public void afterJob(JobExecution jobExecution)
+    {
+        // TODO Auto-generated method stub
+        String jobName = jobExecution.getJobInstance().getJobName();
+        BatchStatus jobStatus = jobExecution.getStatus();
+        String strLog = String.format("[%s] is completed with the status [%s]", jobName, jobStatus);
+        log.info(String.format("[JobLoggerListener/afterJob] %s", strLog));
+    }
+    
+}
---
+++ springboot_study_0010/src/test/java/com/colamanlabs/springbootstudy/s0010/SpringbootStudy0010ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colamanlabs.springbootstudy.s0010;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudy0010ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
