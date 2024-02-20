e53adbd4795d02b2bdfb9f275329fa50c813e45e
DEFAULT3\colaman <colaman@nate.com>
2024-02-20 23:31:48 +0900

    스탭 리스너 테스트

---
+++ springboot_study_0015/src/main/java/com/colamanlabs/springbootstudy/s0015/MySimpleTasklet0003.java
@@ -0,0 +1,30 @@
+package com.colamanlabs.springbootstudy.s0015;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MySimpleTasklet0003 implements Tasklet
+{
+    
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception
+    {
+        log.info(String.format("[MySimpleTasklet0003/execute] BEGIN"));
+        
+        ExecutionContext jobContext = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext();
+        String userName = (String) (jobContext.get("user.name"));
+        
+        log.info(String.format("[MySimpleTasklet0003/execute] userName:[%s]", userName));
+        
+        log.info(String.format("[MySimpleTasklet0003/execute] END"));
+        
+        return RepeatStatus.FINISHED;
+    }
+    
+}
---
+++ springboot_study_0015/src/test/java/com/colamanlabs/springbootstudy/s0015/SpringbootStudy0015ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colamanlabs.springbootstudy.s0015;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudy0015ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
