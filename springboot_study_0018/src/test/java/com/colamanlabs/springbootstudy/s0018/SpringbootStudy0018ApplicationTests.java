5d330b9af969cb5738b82e138e40943495d23246
DEFAULT3\colaman <colaman@nate.com>
2024-02-23 20:39:58 +0900

    5장 검토 환경구성

---
+++ springboot_study_0018/src/test/java/com/colamanlabs/springbootstudy/s0018/SpringbootStudy0018ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colamanlabs.springbootstudy.s0018;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudy0018ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
