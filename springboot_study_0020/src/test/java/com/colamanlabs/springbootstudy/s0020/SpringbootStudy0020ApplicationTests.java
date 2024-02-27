8ea3c6310a01abaa20df686e6ee1626a8e8f258e
colaman <colaman@nate.com>
2024-02-27 12:41:05 +0900

    환경구성

---
+++ springboot_study_0020/src/test/java/com/colamanlabs/springbootstudy/s0020/SpringbootStudy0020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colamanlabs.springbootstudy.s0020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudy0020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
